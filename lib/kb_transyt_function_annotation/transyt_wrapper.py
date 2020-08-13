2965fdce7b93f8b0bda968c7d4e220d0ef4d0f77
Davide Lagoa <davidelagoa@hotmail.com>
2020-08-13 01:31:53 -0500

    models merger created

--- lib/kb_transyt_function_annotation/transyt_wrapper.py
+++ lib/kb_transyt_function_annotation/transyt_wrapper.py
@@ -53,8 +53,7 @@ class transyt_wrapper:
                                                "java.base/jdk.internal.misc=ALL-UNNAMED",
                                                "-Dio.netty.tryReflectionSetAccessible=true", "-Dworkdir=/workdir",
                                                "-Dlogback.configurationFile=/kb/module/conf/logback.xml",
-                                               "-Xmx4096m", self.transyt_jar, "4", self.inputs_path,
-                                               self.results_path])
+                                               "-Xmx4096m", self.transyt_jar, "4", self.inputs_path])
 
         exit_code = transyt_subprocess.wait()
 
